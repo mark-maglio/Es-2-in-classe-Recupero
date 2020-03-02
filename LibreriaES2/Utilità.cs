bcd2b429e628eea6d5af284dd883fd615bf40163
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-03-02 16:16:05 +0100

    Aggiungere i file di progetto.

---
+++ LibreriaES2/Utilità.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaES2
+{
+    public class Utilità
+    {
+        public static bool VerificaInteri(double n)
+        {
+            int n1 = (int)n;
+            if (n1 == n)
+                return true;
+            else
+                return false;
+        }
+        public static int Arrotonda(double n)
+        {
+           int n1 = (int)(n + 0.5);
+            return n1;
+        }
+        public static bool Pari_Dispari(int n)
+        {
+            if (n % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ MarkWPF/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibreriaES2;
+
+namespace MarkWPF
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Calcola_Click(object sender, RoutedEventArgs e)
+        {
+            double n = double.Parse(TxtNumero.Text);
+            int n1;
+            if (Utilità.VerificaInteri(n))
+            {
+                lbl_Tutto.Content = "il numero è intero";
+                n1 = (int)n;
+            }
+            else
+            {
+                lbl_Tutto.Content = "il numero non è intero";
+                n1 =Utilità.Arrotonda(n);
+            }
+            if(Utilità.Pari_Dispari(n1))
+            {
+                lbl_Tutto.Content += "\nIl numero è Pari";
+            }
+            else
+            {
+                lbl_Tutto.Content += "\nIl numero è Dispari";
+            }
+        }
+    }
+}
